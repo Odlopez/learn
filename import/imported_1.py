a0c386a5ff76762bdae0d1b93334421f2aec3aad
Shmyaka <odlopez1@yandex.ru>
2021-04-21 11:15:20 +0300

    import-2

---
+++ import/imported_1.py
@@ -0,0 +1,6 @@
+def greeting():
+    print('Hi, lovers to import files!')
+
+
+if __name__ != '__main__':
+    print('Importing is success')
---
+++ import/imported_2.py
@@ -0,0 +1,13 @@
+def some_method_1():
+    print('Hi I am some_method_1')
+
+
+def some_method_2():
+    print('Hi I am some_method_2')
+
+
+print('Hallo! I\'m done')
+
+
+if __name__ != '__main__':
+    print('Import completed!')
