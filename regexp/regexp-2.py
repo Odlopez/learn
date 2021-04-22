083f055e90d059d1ba006ca3e761ddc1704d92ba
Shmyaka <odlopez1@yandex.ru>
2021-04-22 14:23:19 +0300

    regexp 2

---
+++ regexp/regexp-2.py
@@ -0,0 +1,33 @@
+# https://stepik.org/lesson/24470/step/8?unit=6776
+
+# Вам дана последовательность строк.
+# Выведите строки, содержащие "cat" в качестве слова.
+#
+# Примечание:
+# Для работы со словами используйте группы символов \b и \B.
+# Описание этих групп вы можете найти в документации (https://docs.python.org/3.5/library/re.html).
+
+# Sample Input:
+#
+# cat
+# catapult and cat
+# catcat
+# concat
+# Cat
+# "cat"
+# !cat?
+#
+# Sample Output:
+#
+# cat
+# catapult and cat
+# "cat"
+# !cat?
+
+import sys
+import re
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(r'\bcat\b', line):
+        print(line)
