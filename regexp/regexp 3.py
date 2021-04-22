94e93393ba3161f1bc9c3035db0b20fd6f9e3dfd
Shmyaka <odlopez1@yandex.ru>
2021-04-22 15:22:30 +0300

    add comment to regexp

--- regexp/regexp 3.py
+++ regexp/regexp 3.py
@@ -1,3 +1,20 @@
+# https://stepik.org/lesson/24470/step/9?unit=6776
+#
+# Вам дана последовательность строк.
+# Выведите строки, содержащие две буквы "z﻿", между которыми ровно три символа.
+# Sample Input:
+#
+# zabcz
+# zzz
+# zzxzz
+# zz
+# zxz
+# zzxzxxz
+# Sample Output:
+#
+# zabcz
+# zzxzz
+
 import sys
 import re
 
